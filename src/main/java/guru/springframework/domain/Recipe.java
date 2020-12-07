f5bad63cb30bc6f0285a15c96e45aa7dbdc3efe8
sabdouni <abdouni.souhayl@gmail.com>
2020-12-07 23:33:06 +0100

    Refactoring to use Project Lombok

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,9 +1,12 @@
 package guru.springframework.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Data
 @Entity
 public class Recipe {
     @Id
@@ -22,7 +25,7 @@ public class Recipe {
     //private Difficulty difficulty;
 
     @OneToMany(cascade=CascadeType.ALL, mappedBy="recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     @Lob
     private Byte[] image;
@@ -35,121 +38,15 @@ public class Recipe {
 
     @ManyToMany
     @JoinTable(name="recipe_category", joinColumns=@JoinColumn(name="recipe_id"), inverseJoinColumns = @JoinColumn(name="category_id"))
-    private Set<Category> categories;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getPrepTime() {
-        return prepTime;
-    }
-
-    public void setPrepTime(Integer prepTime) {
-        this.prepTime = prepTime;
-    }
-
-    public Integer getCookTime() {
-        return cookTime;
-    }
-
-    public void setCookTime(Integer cookTime) {
-        this.cookTime = cookTime;
-    }
-
-    public Integer getServings() {
-        return servings;
-    }
-
-    public void setServings(Integer servings) {
-        this.servings = servings;
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getDirections() {
-        return directions;
-    }
-
-    public void setDirections(String directions) {
-        this.directions = directions;
-    }
-
-    public Set<Ingredient> getIngredients() {
-        if(ingredients==null){
-            ingredients = new HashSet<>();
-        }
-        return ingredients;
-    }
-
-    public void setIngredients(Set<Ingredient> ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public Byte[] getImage() {
-        return image;
-    }
-
-    public void setImage(Byte[] image) {
-        this.image = image;
-    }
-
-    public Difficulty getDifficulty() {
-        return difficulty;
-    }
-
-    public void setDifficulty(Difficulty difficulty) {
-        this.difficulty = difficulty;
-    }
-
-    public Notes getNotes() {
-        return notes;
-    }
+    private Set<Category> categories = new HashSet<>();
 
     public void setNotes(Notes notes) {
         notes.setRecipe(this);
         this.notes = notes;
     }
 
-    public Set<Category> getCategories() {
-        if(categories==null){
-            categories = new HashSet<>();
-        }
-        return categories;
-    }
-
-    public void setCategories(Set<Category> categories) {
-        this.categories = categories;
-    }
-
     public void addIngredient(Ingredient ingredient){
         ingredient.setRecipe(this);
-        getIngredients().add(ingredient);
+        ingredients.add(ingredient);
     }
 }
